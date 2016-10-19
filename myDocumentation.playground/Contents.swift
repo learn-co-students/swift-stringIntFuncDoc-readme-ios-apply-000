4e948029d9f16e4885a4eec263bb9ed20fae2a0b
jeremylenz <jeremylenz@gmail.com>
2016-10-19 13:54:02 -0400

    HEY AUDREY

---
+++ myDocumentation.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let name = "Audrey"
+
+name.uppercased()
+
+let uppercaseName = name.uppercased()
+print("\(name)... \(name)... \(uppercaseName)! WAKE UP!!")
+
