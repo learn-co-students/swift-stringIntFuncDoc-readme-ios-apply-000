c2219fd3508a0538eafa8119187052ee9df6c6bf
AndrewLStanley <andrewlstanley@MU9CD17M-2.local>
2016-12-08 09:45:37 -0600

    Why won't Audrey wake up

---
+++ swift_strings_functions_documentation.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let name = "Audrey"
+
+name.uppercased()
+
+let uppercaseName = name.uppercased()
+
+print ("\(name)...\(name)... \(uppercaseName)! WAKE UP!")
