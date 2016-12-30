0041be0226c3c5be06e49222e07afa6f9f4ad445
almusto <ales.musto@gmail.com>
2016-12-30 13:25:26 -0500

    append

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,8 @@ name.uppercased()
 let uppercaseName = name.uppercased()
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+
+var name2 = "Audrey"
+
+name2.append(" Hepbrun")
