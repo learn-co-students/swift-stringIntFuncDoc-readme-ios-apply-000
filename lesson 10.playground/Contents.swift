08342ac0c39916b0edd5a6e4320809a590930f1c
Rodolfo Delgado <rodolfodelgado@The-Dark-Side-2.local>
2017-01-02 20:20:06 -0500

    Adding my created file

---
+++ lesson 10.playground/Contents.swift
@@ -0,0 +1,12 @@
+//RODOLFO DELGADO. 9177565688
+
+
+//how to capitalize letters
+
+let name = "Audrey"
+
+name.uppercased()
+
+print (name)
+print (name.uppercased())
+print("Hey... \(name)...\(name)... \(name.uppercased())! wake up!")
