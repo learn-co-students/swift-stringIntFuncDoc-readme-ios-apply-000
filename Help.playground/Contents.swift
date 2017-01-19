9f16c95a5cd40fee6b4e5ab195f5dd213ce1e752
Michael Watanabe <mikey.watanabe@gmail.com>
2017-01-19 15:46:15 -0500

    Documentation Complete

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,15 @@ name.uppercased()
 let uppercaseName = name.uppercased()
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+let name2 = "Michael"
+
+name2.uppercased()
+
+let uppercaseName2 = name2.uppercased()
+
+func wakeUp() {
+    print("\(name2) are you listening to me? \(uppercaseName2)!!!")
+}
+
+wakeUp()
