abb6599d9352446e105df514df8f10a1b608b720
burakflatiron <burakflatiron@Tech-Book.home>
2017-02-05 13:32:43 -0500

    Finished Strings,Their functions, and Documentation

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,14 @@ name.uppercased()
 let uppercaseName = name.uppercased()
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+let burakName = "Burak"
+
+burakName.lowercased()
+burakName.uppercased()
+
+let burakNameUpperCased = burakName.uppercased()
+let burakNameLowerCased = burakName.lowercased()
+
+print("Upper:\(burakNameUpperCased), Lower:\(burakNameLowerCased)")
+
