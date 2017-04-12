81df00168ad826b514780b839a27b3b35d3797a0
“Diljeet <jeetds1994@gmail.com>
2017-04-11 21:22:09 -0400

    added my own lowercase String

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,10 @@ name.uppercased()
 let uppercaseName = name.uppercased()
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+
+let name2 = "Adrian"
+
+let lowercaseName = name2.lowercased()
+
+print("\(name2)... \(name2)... \(lowercaseName)..... go to sleep....zzzzz")
