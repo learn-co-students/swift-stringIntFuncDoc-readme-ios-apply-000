47cd8e22ef32fc0226750879b3062cf0c9b72d1b
gpeligro <marthom9@gmail.com>
2017-04-18 08:18:51 +0200

    Strings, Their Functions, and Documentation -DONE

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -1,6 +1,21 @@
+
 let name = "Audrey"
+
+// A String is a sequence of characters, either as literal constant (let) or as some kind of variable (var)
+
+print(name)
+name
+
 name.uppercased()
 
-let uppercaseName = name.uppercased()
-print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
-// prints "Audrey... Audrey... AUDREY! WAKE UP!")
+print(name.uppercased())
+name.uppercased()
+
+func wakeUp()
+{
+    let upperCaseName = name.uppercased()
+    print("\(name)...\(name)...\(upperCaseName)! WAKE UP!")
+
+}
+
+
